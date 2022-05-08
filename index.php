ec594522db8b8240d95fc939cef9dea2a543b48e
Giovanni Riefolo <giovanni.riefolo@heply.it>
2022-05-08 11:49:47 +0200

    add base files

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * WordPress requires a blank index.php to activate themes.
+ * Just leave it empty and have fun!
+ *
+ * @link https://core.trac.wordpress.org/ticket/54272
+ *
+ * @package WP Trek FSE
+ * @since 1.0.0
+ */
